b1a839ce437232f063e1e7c20525a41af65b0cbe
admin <admin@mmylashko-mac-2.local>
2019-07-02 16:55:20 +0300

    first commit

---
+++ RequestsExecutionManager.h
@@ -0,0 +1,19 @@
+//
+//  RequestsExecutionManager.h
+//  RequestsExecutionManager
+//
+//  Created by admin on 7/2/19.
+//  Copyright © 2019 MaksymOdesa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RequestsExecutionManager.
+FOUNDATION_EXPORT double RequestsExecutionManagerVersionNumber;
+
+//! Project version string for RequestsExecutionManager.
+FOUNDATION_EXPORT const unsigned char RequestsExecutionManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RequestsExecutionManager/PublicHeader.h>
+
+
